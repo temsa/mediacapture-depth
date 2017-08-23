60edf1e10dca0541778c25c0acc3276e20b6b683
Florian Traverse <florian.traverse@rocksolead.com>
2017-08-23 17:47:39 +0200

    split the script in another file

---
+++ index.js
@@ -0,0 +1,30 @@
+function failure(err) {
+  console.error('failure!', err)
+}
+
+function success(s) {
+  console.log('success !',s)
+  // wire the stream into a <video> element for playback
+  // ISSUE: should the implementations allow the user to
+  // switch between the RGB video and depth stream playback
+  // if the stream contains both video and stream tracks?
+  var video = document.querySelector('#video');
+  video.src = URL.createObjectURL(s);
+  video.play();
+
+  // construct a new MediaStream out of the existing depth track(s)
+  var depthStream = new MediaStream(s.getDepthTracks()[0]);
+
+  // (not supported, for future work)
+  // send the newly created depth stream over a RTCPeerConnection
+  // var peerConnection = new RTCPeerConnection(config);
+  // peerConnection.addStream(depthStream);
+
+  // wire the depth stream into another <video> element for playback
+  // NOTE: how the depth information is visualized as a 8-bit grayscale representation
+  var depthVideo = document.querySelector('#depthVideo');
+  depthVideo.src = URL.createObjectURL(depthStream);
+  depthVideo.play();
+}
+
+navigator.mediaDevices.getUserMedia({ video: true, depth: true }, success, failure);
