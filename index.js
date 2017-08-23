2fc064a0f7882ff03dad33861644cb252f0d1e14
Florian Traverse <florian.traverse@rocksolead.com>
2017-08-23 17:56:41 +0200

    cosmetics

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function failure(err) {
+function failure (err) {
   console.error('failure!', err)
 }
 
-function success(s) {
-  console.log('success !',s)
+function success (s) {
+  console.log('success !', s)
   // wire the stream into a <video> element for playback
   // ISSUE: should the implementations allow the user to
   // switch between the RGB video and depth stream playback
   // if the stream contains both video and stream tracks?
-  var video = document.querySelector('#video');
-  video.src = URL.createObjectURL(s);
-  video.play();
+  var video = document.querySelector('#video')
+  video.src = URL.createObjectURL(s)
+  video.play()
 
   // construct a new MediaStream out of the existing depth track(s)
-  var depthStream = new MediaStream(s.getDepthTracks()[0]);
+  var depthStream = new MediaStream(s.getDepthTracks()[0])
 
   // (not supported, for future work)
   // send the newly created depth stream over a RTCPeerConnection
@@ -22,9 +22,9 @@ function success(s) {
 
   // wire the depth stream into another <video> element for playback
   // NOTE: how the depth information is visualized as a 8-bit grayscale representation
-  var depthVideo = document.querySelector('#depthVideo');
-  depthVideo.src = URL.createObjectURL(depthStream);
-  depthVideo.play();
+  var depthVideo = document.querySelector('#depthVideo')
+  depthVideo.src = URL.createObjectURL(depthStream)
+  depthVideo.play()
 }
 
 navigator.mediaDevices.getUserMedia({ video: true, depth: true })
